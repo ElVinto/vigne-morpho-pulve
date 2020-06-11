7788e1ea32cf1f6bacc27be7c3377aa3ee6d8fa6
Vincent Armant <vincent.armant@insight-centre.org>
2020-06-11 17:38:45 +0200

    updated picture and objective welcome page

--- client/src/components/Welcome.vue
+++ client/src/components/Welcome.vue
@@ -1,27 +1,39 @@
 <template>
   <div
     class="global"
-    style="background-image:url(images/AniceCheraiet-MasPiquet-LIDAR_c_INRAE-CaroleGiansily-1920x1440px.jpg) ;background-size: 2200px 800px;padding:30px;margin: 1px;height: 800px">
+    style="background-image:url(images/AniceCheraiet-MasPiquet-LIDAR_c_INRAE-CaroleGiansily-4032x3024px.jpg); background-size: 2200px 800px;padding:30px;margin: 1px;height: 800px">
  
-  <div id="title"><h1>Vigne morpho pulve</h1></div>
+  <div id="title">
+    <h1>Vigne morpho pulve</h1>
+  </div>
      
     
 <hr>
 	
-	<div id="objective" class="card bg-light text-white" style="opacity:50%">
+	<div id="objective" class="card bg-dark text-white" style="opacity:90%">
       
       <div class="card-body" >
-        <center><b style="color:black;">Objectif</b></center>
-        <p style="color:black;"> a remplir</p>
+        <p>
+        <center><b >
+          Déterminer la juste dose de phytosanitaire à appliquer en fonction du stade de végétation de la vigne et du pulvérisateur utilisé
+          </b></center>
+        </p>
+        <p > 
+          En fonction du type de pulvérisateur, du stade phénologique de la vigne, de la canopée, l’application détermine la juste dose de produit phytosanitaire à appliquer et la compare avec la dose maximale proposée par défaut. 
+          Les données d’entrées sont la parcelle, le cépage, le stade phénologique, l’applicateur utilisé.
+        </p>
+        <p>Développeur(s) : Vincent Armant et Amin Ouail | Doctorant : Anice Cheraiet, ITAP, INRAE</p>
       </div>
     </div>
     
 
 
-    <div id="btnScenario" style="width: auto;">
+    <div id="btnScenario" style="width: auto; opacity:90%;">
     <b-button to="/scenario" variant="dark" style="width: auto;"><img src="images/scenario.png"/><br><b>Sélectionner un scénario</b></b-button>     
     </div>
 
+
+  <!--
 	<div id="btnGuide">
      <b-button id="btnGuide" variant="dark" v-b-toggle.sidebar-footer style="opacity:80%">
         <img src="images/guid.png"/><br>Mode d'emploi</b-button>
@@ -34,16 +46,11 @@
         <p>
          <h5>Bienvenue sur l'application </h5><br>
 
-       a remplir
       </div>
     </b-sidebar>
-
-    
+  -->
+</div>
   
-
- 
-    
-      </div>
    
 </template>
 <script>
@@ -73,6 +80,14 @@ export default {
 <style scoped>
 
 
+.card-body {  
+  overflow-y: scroll; 
+  width: 100%; 
+  text-align: justify; 
+  margin-top: 0px;
+  font-size: small;
+  }
+
 
 @media screen and (min-width: 900px) {
 .global {
@@ -83,13 +98,18 @@ export default {
  
 #title {color:rgb(105, 103, 101);
   grid-column: 1/6;
-  grid-row: 1 / 9;
+  grid-row: 1;
 }
 
+#btnScenario { 
+  width: auto;
+  grid-column: 3 ;
+  grid-row: 2;
+}
 
 #objective {
-  grid-column: 3;
-  grid-row: 2 / 5;
+  grid-column: 2/5;
+  grid-row: 5/6;
 }
 
 #btnGuide {
@@ -97,11 +117,7 @@ export default {
   grid-row: 5;
 }}
 
-#btnScenario { 
-  width: auto;
-  grid-column: 3 ;
-  grid-row: 6;
-}
+
 
 
 </style>
